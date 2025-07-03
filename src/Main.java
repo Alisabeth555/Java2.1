5d2f96f4c54e2f8dd8351a5a2bf307fe10bc5e87
Alisabeth <pisarevalisabeth@gmail.com>
2025-07-03 16:49:07 +0300

    create program

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        {
+            int amount = 15794;
+            int bonus;
+
+            if (amount >= 20)
+                bonus = amount / 20;
+            else bonus = 0;
+
+            System.out.println("начислено бонусов " + (bonus));
+        }
+    }
+}
+
