d1d14626363abfe64182c3c2d0a11802b6bcd78c
Alisabeth555 <pisarevalisabeth@gmail.com>
2025-07-05 18:28:08 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
         
             int amount = 15794; //стоимость билета
-            int a = 20; // количество рублей для одной бонусной мили
+            int step = 20; // количество рублей для одной бонусной мили
             int bonus;
-
-            if (amount >= (a))
-                bonus = amount / a;
+        {
+            if (amount >= (step))
+                bonus = amount / step;
             else bonus = 0;
-
+        }
             System.out.println("начислено бонусов " + (bonus));
         
     }
