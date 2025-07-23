d0d3764f2c28e5b045246fb510fb16d84e17237c
Alisabeth <pisarevalisabeth@gmail.com>
2025-07-23 18:37:27 +0300

    Modif

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
         int amount = 15794; //стоимость билета
         int step = 20; // количество рублей для одной бонусной мили
         int bonus;
-        {
-            if (amount >= (step))
-                bonus = amount / step;
-            else bonus = 0;
+        if (amount >= (step)) {
+            bonus = amount / step;
+        } else {
+            bonus = 0;
         }
         System.out.println("начислено бонусов " + (bonus));
 
